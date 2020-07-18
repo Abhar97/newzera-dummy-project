eaa44d2bea9e90959ada70e7f2dcc2cca94bbf4c
Abhar97 <akashbhardwajgkp1997@gmail.com>
2020-07-19 00:24:20 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -1,19 +1,22 @@
 var slideIndex = 1;
 showSlides(slideIndex);
 
+var n;
+
 function currentSlide(n){
     showSlides(slideIndex=n);
 }
 
 function showSlides(slideIndex){
     var i;    
+    var n = slideIndex;
     var slides = document.getElementsByClassName("myslides");
     var dots = document.getElementsByClassName("dot");
     if(n>slides.length) {slideIndex=1;}
     if(n<1) {slideIndex = slides.length;}
 
     for(i=0;i<slides.length;i++){
-        slides[i].style.display = none;
+        slides[i].style.display = "none";
     }
 
     for (i = 0; i < dots.length; i++) {
@@ -21,4 +24,5 @@ function showSlides(slideIndex){
     }
     slides[slideIndex-1].style.display = "block";  
     dots[slideIndex-1].className += " active";
+    console.log("success");
 }
